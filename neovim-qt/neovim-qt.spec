1254a4f91367198ef90700eb164c957c1c9c1800
Maciek Krawiec <maciek.krawiec@zoho.com>
2016-04-16 10:06:26 +0200

    Bump neovim-qt to 0.1.2

--- neovim-qt/neovim-qt.spec
+++ neovim-qt/neovim-qt.spec
@@ -1,5 +1,5 @@
 Name:           neovim-qt
-Version: 0.1.1
+Version: 0.1.2
 Release:        1%{?dist}
 Summary:        Neovim RPC and GUI using Qt5
 
--- neovim-qt/service.sh
+++ neovim-qt/service.sh
@@ -1,4 +1,4 @@
-PKG_VERSION=0.1.1
+PKG_VERSION=0.1.2
 PKG_DOWNLOAD_URL=https://github.com/equalsraf/neovim-qt/archive/v${PKG_VERSION}.tar.gz
 
 pkg_prebuild_hook()
